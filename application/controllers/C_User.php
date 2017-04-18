baf61ac5f013451c27575c786ce6659c7ab94f01
fabiyanu ramadani <fabiyanu ramadani>
2017-04-18 15:48:18 +0700

    Insert User Work

--- application/controllers/C_User.php
+++ application/controllers/C_User.php
@@ -3,6 +3,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class C_User extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model("M_entry");
+    }
 
     public function index() {
 
@@ -14,12 +19,18 @@ class C_User extends CI_Controller {
     }
     
     function tambahdata(){
- 
-        if($this->input->post('submit')){
-            $this->load->model('M_entry');
-            $this->M_entry->tambah();
-            redirect('C_User');
-        }
-        $this->load->view('V_User');
+        $d['id_jabatan'] = $this->input->post('jabatan');
+        $d['nama'] = $this->input->post('nama');
+        $d['jenis_kelamin'] = $this->input->post('jk');
+        $d['divisi'] = $this->input->post('divisi');
+        $d['tanggal_lahir'] = $this->input->post('tgl');
+        $d['alamat'] = $this->input->post('alamat');
+        $d['nomor_telp'] = $this->input->post('nomer');
+        $d['username'] = $this->input->post('username');
+        $d['password'] = $this->input->post('password');
+        
+        $this->M_entry->tambah($d);
+        
+        header("location:".base_Url('/index.php/C_User'));
     }
 }
--- application/models/M_entry.php
+++ application/models/M_entry.php
@@ -1,27 +1,6 @@
 <?php
 class M_entry extends CI_Model {
-    function tambah(){
-        $nama = $this->input->post('nama');
-        $jenis_k = $this->input->post('jenis_k');
-        $divisi = $this->input->post('divisi');
-        $tgl_lahir = $this->input->post('tgl_lahir');
-        $alamat = $this->input->post('alamat');
-        $no_hp = $this->input->post('no_hp');
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        
-        $data = array(
-                'id_user'=>'1',
-                'id_jabatan'=>'2',
-                'nama'=>$nama,
-                'jenis_kelamin'=>$jenis_k,
-                'divisi'=>$divisi,
-                'tanggal_lahir'=>$tgl_lahir,
-                'alamat'=>$alamat,
-                'nomor_telp'=>$no_hp,
-                'username'=>$username,
-                'password'=>$password,
-                );
+    function tambah($data){        
         $this->db->insert('m_user',$data);
     }
 }
